57a10eea47bdd586263b328a105be082a5d5e00b
Adam Warski <adam@warski.org>
2009-10-14 14:59:06 +0200

    Removing unused div-by-zero example plugin

--- example/src/test/scala/Example1.scala
+++ example/src/test/scala/Example1.scala
@@ -3,9 +3,9 @@ import javax.annotation.Nonnull
 /**
  * @author Adam Warski (adam at warski dot org)
  */
-
 object Example1 {
   def parameterMustBeNull(@Nonnull parameter : Object)(param2 : Object): Int = {
+    // Here an if should be generated, which checks if the parameter is null.
     println("Shouldn't get here!")
     1
   }
